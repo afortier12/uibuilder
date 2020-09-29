2e47768167bb0ffc1f68c3baf443c4977e64abff
Adam Fortier <afortier@teamintegrity.com>
2020-09-29 16:26:01 -0400

    working up to download, need finish kitimport

--- src/download-component.vue
+++ src/download-component.vue
@@ -5,20 +5,32 @@
 		<p>Kardex import directory</p>
 		
 		<b-input-group prepend="Network file path" class="mt-3">
-			<b-form-input :disabled="!enabled" v-model="updatedPath" type="text" :placeholder="path" @input="pathChange"></b-form-input>
+			<b-form-input :disabled="!enabled" v-model="updatedPath" :state="pathState" type="text" :placeholder="path" @input="pathChange"></b-form-input>
+            <b-form-invalid-feedback id="path-feedback">
+                Invalid path - must be of the form '\\server\path'!
+            </b-form-invalid-feedback>
 		</b-input-group>
 		<b-input-group prepend="Job import directory" class="mt-3">
-			<b-form-input :disabled="!enabled" v-model="updatedJobFolder" type="text" :placeholder="job_folder"></b-form-input>
+			<b-form-input :disabled="!enabled" v-model="updatedJobFolder" :state="jobState" type="text" :placeholder="job_folder"></b-form-input>
+			<b-form-invalid-feedback id="job-feedback">
+                Invalid path - letters numbers and underscore only!
+            </b-form-invalid-feedback>
 		</b-input-group>
 		<b-input-group prepend="Bin import directory" class="mt-3">
-			<b-form-input :disabled="!enabled" v-model="updatedBinFolder" type="text" :placeholder="bin_folder"></b-form-input>
+			<b-form-input :disabled="!enabled" v-model="updatedBinFolder" :state="binState" type="text" :placeholder="bin_folder"></b-form-input>
+			<b-form-invalid-feedback id="bin-feedback">
+                Invalid path - letters numbers and underscore only!
+            </b-form-invalid-feedback>
 		</b-input-group>
 		<b-input-group prepend="Kit import directory" class="mt-3">
-			<b-form-input :disabled="!enabled" v-model="updatedKitFolder" type="text" :placeholder="kit_folder" ></b-form-input>
+			<b-form-input :disabled="!enabled" v-model="updatedKitFolder" :state="kitState" type="text" :placeholder="kit_folder" ></b-form-input>
+			<b-form-invalid-feedback id="kit-feedback">
+                Invalid path - letters numbers and underscore only!
+            </b-form-invalid-feedback>
 		</b-input-group>
 		
 		<p></p>
-		<b-button :disabled="!enabled" variant="getState" click="download()">{{ button_text }}</b-button>
+		<b-button :disabled="!enabled" :variant="getState" v-on:click="download">{{ button_text }}</b-button>
         </b-form>
         
         <p></p>
@@ -56,9 +68,13 @@ module.exports = {
 			currentStatus: null,
 			uploadFieldName: 'File',
 			updatedPath : '',
+			pathState: null,
 			updatedKitFolder : '',
+			kitState : null,
 			updatedJobFolder : '',
+			jobState : null,
 			updatedBinFolder : '',
+			binState : null,
 		}
     }, // --- End of data --- //
     computed: {
@@ -73,9 +89,53 @@ module.exports = {
 	    }
     }, // --- End of computed --- //
     methods: {
-        download(formData) {
+        download() {
 			// upload data to the server
 			this.currentStatus = STATE_SAVING;
+			
+			var new_path = this.path;
+			if (this.updatedPath.length > 0) new_path = this.updatedPath;
+			var new_kitFolder = this.kit_folder;
+			if (this.updatedKitFolder.length > 0) new_path = this.updatedKitFolder;
+			var new_jobFolder = this.job_folder;
+			if (this.updatedJobFolder.length > 0) new_path = this.updatedJobFolder;
+			var new_binFolder = this.bin_folder;
+			if (this.updatedBinFolder.length > 0) new_path = this.updatedBinFolder;
+			
+			if (new_path.match(/^(\\)(\\[A-Za-z0-9-_]+){2,}$/) === null){
+			    this.pathState = false;
+			} else {
+			    this.pathState = true;
+			}
+			if(new_kitFolder.match(/^([A-Za-z0-9-_]+[^\\]$)/) === null){
+			    this.kitState = false;
+			} else {
+			    this.kitState = true;
+			}
+			if(new_jobFolder.match(/^([A-Za-z0-9-_]+[^\\]$)/) === null){
+			    this.jobState = false;
+			} else {
+			    this.jobState = true;
+			}
+			if(new_binFolder.match(/^([A-Za-z0-9-_]+[^\\]$)/) === null){
+			    this.binState = false;
+			} else {
+                this.binState = true;
+			}
+			    
+			if (this.pathState && this.kitState && this.jobState && this.binState){   
+			    var topic = 'download';
+			    uibuilder.send( {
+                    'topic': topic,
+                    'payload': {
+                        'path': new_path,
+                        'kitFolder': new_kitFolder,
+                        'jobFolder': new_jobFolder,
+                        'binFolder': new_binFolder
+                    }
+                })
+			    
+			}
 
 	    },
         pathChange(evt) {
@@ -85,7 +145,7 @@ module.exports = {
         },
     }, // --- End of methods --- //
     watch: {
-        
+       
     },	// --- End of watches --- //
     // Available hooks: init,mounted,updated,destroyed
     mounted: function(){
@@ -113,7 +173,7 @@ module.exports = {
 
     }, // --- End of mounted hook --- //
     init: function(){
-        this.updatePath = this.path;
+        this.updatedPath = this.path;
     }
 
 } 
